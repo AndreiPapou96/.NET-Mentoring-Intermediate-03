2094101b0b9981540bec61846857bb7305a865bd
a_papou <andrei.papou96@gmail.com>
2021-06-14 19:03:22 +0300

    init

---
+++ MessageClient/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Messaging;
+using System.IO;
+
+namespace MessageClient
+{
+    class Program
+    {
+        const string MessageQueueName = @".\private$\MyTransactionalQueue";
+        const string FolderPath = @"C:\Users\Andrei_Papou\Listen_Folder";
+        const int BUFFER_SIZE = 3;
+        static MessageQueue queue;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Client has started...");
+
+            if (MessageQueue.Exists(MessageQueueName))
+                queue = new MessageQueue(MessageQueueName);
+            else
+                queue = MessageQueue.Create(MessageQueueName);
+
+            queue.Formatter = new XmlMessageFormatter(new Type[] { typeof(byte[]) });
+
+            FileSystemWatcher watcher = new FileSystemWatcher(FolderPath);
+
+            watcher.EnableRaisingEvents = true;
+            watcher.Filter = "*.zip";
+            watcher.Created += SendFileToQueue;
+
+            Console.ReadLine();
+        }
+
+        static void SendFileToQueue(object sender, FileSystemEventArgs e)
+        {
+            if (queue.Transactional == true)
+            {
+                // Create a transaction.
+                MessageQueueTransaction myTransaction = new
+                    MessageQueueTransaction();
+
+                // Begin the transaction.
+                myTransaction.Begin();
+
+                byte[] buffer = new byte[BUFFER_SIZE];
+
+                using (var input = new FileStream(e.FullPath, FileMode.Open, FileAccess.Read))
+                {
+                    int index = 0;
+
+                    while (input.Position < input.Length)
+                    {
+                        int remaining = 3, bytesRead;
+                        while (remaining > 0 && (bytesRead = input.Read(buffer, 0,
+                                Math.Min(remaining, BUFFER_SIZE))) > 0)
+                        {
+                            queue.Send(buffer, e.Name, myTransaction);
+                            remaining -= bytesRead;
+                        }
+
+                        index++;
+                    }
+                }
+
+                // Commit the transaction.
+                myTransaction.Commit();
+            }
+            else
+            {
+                queue.Send("My Message Data.");
+            }
+        }
+    }
+}
---
+++ MessageServer/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Messaging;
+using System.IO;
+
+namespace MessageServer
+{
+    class Program
+    {
+        const string MessageQueueName = @".\private$\MyTransactionalQueue";
+        const string FolderPath = @"C:\Users\Andrei_Papou\result_folder";
+        static MessageQueue queue;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Server has started...");
+
+            if (MessageQueue.Exists(MessageQueueName))
+                queue = new MessageQueue(MessageQueueName);
+            else
+                queue = MessageQueue.Create(MessageQueueName, true);
+
+            queue.Formatter = new XmlMessageFormatter(new Type[] { typeof(byte[]) });
+            queue.MessageReadPropertyFilter = new MessagePropertyFilter
+            {
+                IsLastInTransaction = true,
+                Label = true,
+                Body = true,
+            };
+
+            Task.Run(ListenQueue);
+
+            Console.ReadLine();
+
+        }
+
+        static void ListenQueue()
+        {
+            while (true)
+            {
+                using (var trans = new MessageQueueTransaction())
+                {
+                    try
+                    {
+                        var chunks = new List<byte>();
+                        var fileName = string.Empty;
+                        var index = 0;
+
+                        trans.Begin();
+                        Message message;
+
+                        while ((message = queue.Receive(trans)) != null)
+                        {
+                            var chunk = (byte[])message.Body;
+                            fileName = message.Label;
+                            chunks.AddRange(chunk);
+
+                            Console.WriteLine($"{fileName} - #{index}");
+                            index++;
+
+                            if (message.IsLastInTransaction)
+                            {
+                                break;
+                            }
+                        }
+
+                        trans.Commit();
+                        CombineChunksIntoSingleFile(chunks, FolderPath, fileName);
+                    }
+
+                    catch (MessageQueueException e)
+                    {
+                        if (e.MessageQueueErrorCode ==
+                            MessageQueueErrorCode.TransactionUsage)
+                        {
+                            Console.WriteLine("Queue is not transactional.");
+                        }
+
+                        trans.Abort();
+                    }
+                }
+            }
+        }
+        static void CombineChunksIntoSingleFile(List<byte> chunks, string folderPath, string fileName)
+        {
+            using (var input = new FileStream($"{folderPath}\\{fileName}", FileMode.Create, FileAccess.Write))
+            {
+                foreach (byte chunk in chunks)
+                {
+                    input.WriteByte(chunk);
+                }
+            }
+        }
+    }
+}
